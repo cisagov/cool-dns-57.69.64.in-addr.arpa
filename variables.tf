5c11ffd7ca5f8b1346f9d1a011c0f8754895eb33
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-09-25 15:42:04 -0400

    Update variables.tf
    Sort the variables alphabetically and ensure all variables have a
    `type` attribute.

--- variables.tf
+++ variables.tf
@@ -16,6 +16,18 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "route53resourcechange_role_description" {
+  type        = string
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient permissions to modify resource records in the DNS zone."
+  default     = "Allows sufficient permissions to modify resource records in the DNS zone."
+}
+
+variable "route53resourcechange_role_name" {
+  type        = string
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient permissions to modify resource records in the DNS zone."
+  default     = "Route53ResourceChange-57.69.64.in-addr.arpa"
+}
+
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources created."
@@ -24,14 +36,4 @@ variable "tags" {
     Application = "COOL - DNS - 57.69.64.in-addr.arpa"
     Workspace   = "production"
   }
-}
-
-variable "route53resourcechange_role_description" {
-  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows sufficient permissions to modify resource records in the DNS zone."
-  default     = "Allows sufficient permissions to modify resource records in the DNS zone."
-}
-
-variable "route53resourcechange_role_name" {
-  description = "The name to assign the IAM role (as well as the corresponding policy) that allows sufficient permissions to modify resource records in the DNS zone."
-  default     = "Route53ResourceChange-57.69.64.in-addr.arpa"
 }
